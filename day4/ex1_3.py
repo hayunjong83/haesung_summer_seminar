009c5b48bd3f93a5741aa029b23669fb7be00c40
hayunjong83 <hayunjong83@gmail.com>
2025-07-25 08:20:23 +0900

    update

--- day4/ex1_3.py
+++ day4/ex1_3.py
@@ -8,15 +8,9 @@ fps = cap.get(cv2.CAP_PROP_FPS)             # 초당 재생속도(fps)
 w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))  # 영상의 가로길이(너비)
 h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)) # 영상의 세로길이(높이)
 
-# 관찰할 영역을 지정한다.
-region_points = {
-  "region-01" : [(430, 150), (460, 130), (580, 130), (550, 150)],
-  "region-02" : [(200, 220), (170, 190), (230, 190), (260, 220)],
-}
-
 counter = solutions.ObjectCounter(
   show=True,
-  region=[(430, 150), (550, 150)],
+  region=[(430, 150), (550, 150)],     # 관찰할 영역을 지정한다.
   model = "yolo11l.pt",
   # classes = [2],
 )
